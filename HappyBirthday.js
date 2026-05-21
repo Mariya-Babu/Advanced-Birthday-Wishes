422ceebc9adaa956addd59668cc21ba14e9bef5d
Ravikumar1032 <ravikumarnaik1015@gmail.com>
2026-05-21 22:11:00 +0530

    made the website to allow dynamic values

--- HappyBirthday.js
+++ HappyBirthday.js
@@ -1,16 +1,31 @@
-var count = new Date("Oct 26, 2024 00:00:00").getTime();
+// Get URL parameters
+const params = new URLSearchParams(window.location.search);
 
-var x = setInterval(function() {
+// Default values
+const defaultName = "Mariya Babu.Com";
+const defaultDOB = "July 14 2027";
+
+// Read values from URL or use defaults
+const name = params.get("name") || defaultName;
+const dob = params.get("dob") || defaultDOB;
+
+// Show name in HTML
+document.querySelector("h2").innerHTML = `${name} 🔥`;
+
+// Countdown Date
+var count = new Date(dob + " 00:00:00").getTime();
+
+var x = setInterval(function () {
 
     var now = new Date().getTime();
 
     var distance = count - now;
-    
+
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
+
     document.getElementById("days").innerHTML = days;
     document.getElementById("hours").innerHTML = hours;
     document.getElementById("minutes").innerHTML = minutes;
@@ -18,6 +33,6 @@ var x = setInterval(function() {
 
     if (distance < 0) {
         clearInterval(x);
-        //document.getElementById("demo").innerHTML = "EXPIRED";
     }
+
 }, 1000);
